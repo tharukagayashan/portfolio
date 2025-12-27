6384d0fe31db4e0d705f7de21fdb944d01a159cb
Tharuka Gayashan <tharuka.gayashan.5@gmail.com>
2025-12-27 19:31:50 +0530

    new changes

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    base: "/",
+    base: "/portfolio/",
     host: "::",
     port: 8080,
   },
